8867d960a431fb11e9382bb18c813ee89151bf91
Kyaw Myo Htay <kyawmyohtay2004@gmail.com>
2025-09-23 23:21:21 +0630

    password required validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,12 +46,8 @@ class UserController extends Controller
         $validated = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user->id)],
-            'password' => ['nullable', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
-
-        if (empty($validated['password'])) {
-            unset($validated['password']);
-        }
 
         $user->update($validated);
 
